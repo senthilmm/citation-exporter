ef412813d9316542f96c34cb272865008b13ac8d
Maloney, Christopher <maloneyc@ncbi.nlm.nih.gov>
2014-04-14 17:53:30 -0400

    PMC-20077 - id resolving.  start integrating kitty-cache

--- src/main/java/gov/ncbi/pmc/cite/IdResolver.java
+++ src/main/java/gov/ncbi/pmc/cite/IdResolver.java
@@ -13,29 +13,47 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.spaceprogram.kittycache.KittyCache;
 
 
 public class IdResolver {
     // The keys here are the ID strings.  The sets of IDs for each type are assumed to
     // be disjoint.
     protected Map<String, String> idCache;
+    // FIXME: move to KittyCache:
+    KittyCache<String, String> kCache;
+
     ObjectMapper mapper = new ObjectMapper(); // create once, reuse
 
+    // FIXME:  these config variables should be in system properties
+    public boolean cacheIds = false;
+    public int cacheTimeToLive = 0;
+
+    // FIXME:  this should be in a system property
     public String idConverterUrl = "http://web.pubmedcentral.nih.gov/utils/idconv/v1.1/?showaiid=yes&format=json&";
 
     // Here we specify the regexp patterns that will be used to match IDs to their type
     // The order is important:  if determining the type of an unknown id (getIdType()), then these
     // regexps are attempted in order, and first match wins.
     protected static String[][] idTypePatterns = {
-            { "pmcid", "^PMC\\d+(\\.\\d+)?$" },
-            { "pmid", "^\\d+$" },
-            { "mid", "^[A-Z]+\\d+$" },
-            { "doi", "^10\\.\\d+\\/.*$" },
-            { "aiid", "^\\d+$" },
+        { "pmcid", "^PMC\\d+(\\.\\d+)?$" },
+        { "pmid", "^\\d+$" },
+        { "mid", "^[A-Z]+\\d+$" },
+        { "doi", "^10\\.\\d+\\/.*$" },
+        { "aiid", "^\\d+$" },
     };
 
     public IdResolver() {
-        idCache = new HashMap<String, String>();
+        if (cacheIds) {
+            idCache = new HashMap<String, String>();
+        }
+        // Create a new cache; 5000 is max number of objects
+        kCache = new KittyCache<String, String>(5000);
+
+        // Put an object into the cache
+        kCache.put("mykey", "Blah", 500); // 500 is time to live in seconds
+        // Get an object from the cache
+        System.out.println("kitty-cache result: " + kCache.get("mykey"));
     }
 
     /**
@@ -112,19 +130,30 @@ public class IdResolver {
             return idSet;
         }
 
-        // Resolve IDs to pmids or aiids, if necessary
+        // Resolve IDs to pmids or aiids, if necessary.  We'll first aggregate the list of
+        // IDs that need to be resolved, so we only have to make one backend call.  Go through
+        // the list and, for each ID, if it is not in the cache, add it to the list.
+        Map<String, String> resolvedIds = new HashMap<String, String>();
         List<String> idsToResolve = new ArrayList<String>();
+
+        System.out.println("Going through the list of IDs");
         for (String id: ids_array) {
-            if (!idCache.containsKey(id)) {
+            if (cacheIds && idCache.containsKey(id)) {
+                resolvedIds.put(id, idCache.get(id));
+            }
+            else {
                 idsToResolve.add(id);
             }
         }
 
+        System.out.println("Need to resolve " + idsToResolve.size() + " ids");
         if (idsToResolve.size() > 0) {
             // Need to call the id resolver
             String idsParam = StringUtils.join(idsToResolve, ",");
             URL url = new URL(idConverterUrl + "idtype=" + idType + "&ids=" + idsParam);
             System.out.println("About to invoke '" + url + "'");
+            // FIXME:  we should use citeproc-java's json library, instead of Jackson,
+            // since we already link to it.
             ObjectNode idconvResponse = (ObjectNode) mapper.readTree(url);
             String status = idconvResponse.get("status").asText();
             System.out.println(idconvResponse);
@@ -134,17 +163,20 @@ public class IdResolver {
             System.out.println(records);
             for (int rn = 0; rn < records.size(); ++rn) {
                 ObjectNode record = (ObjectNode) records.get(rn);
-                // FIXME:  not all types implemented here yet.
-                String pmcid = record.get("pmcid").asText();
+                // FIXME:  Need some error handling
+                String origId = record.get(idType).asText();
                 String aiid = record.get("aiid").asText();
-                idCache.put(record.get(idType).asText(), aiid);
+                resolvedIds.put(origId, aiid);
+                if (cacheIds) {
+                    idCache.put(origId, aiid);
+                }
             }
         }
 
-        IdSet ids = new IdSet("aiid");
+        IdSet idSet = new IdSet("aiid");
         for (String id: ids_array) {
-            ids.ids.add(idCache.get(id));
+            idSet.ids.add(resolvedIds.get(id));
         }
-        return ids;
+        return idSet;
     }
 }
