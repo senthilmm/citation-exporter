302044fc55788b27e5031a7d7e96eedbbcce275b
Chris Maloney <maloneyc@ncbi.nlm.nih.gov>
2015-12-18 23:45:46 -0500

    PMC-20053 - got schematron working to validate json

--- src/test/java/gov/ncbi/pmc/cite/test/CasesTest.java
+++ src/test/java/gov/ncbi/pmc/cite/test/CasesTest.java
@@ -3,26 +3,33 @@ package gov.ncbi.pmc.cite.test;
 import static gov.ncbi.pmc.cite.test.TestUtils.serializeXml;
 import static org.junit.Assert.assertTrue;
 
+import java.io.StringReader;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.helger.commons.xml.serialize.read.DOMInputStreamProvider;
 import com.helger.schematron.ISchematronResource;
 import com.helger.schematron.pure.SchematronResourcePure;
 
 import gov.ncbi.pmc.cite.App;
+import gov.ncbi.pmc.cite.BadParamException;
 import gov.ncbi.pmc.cite.ItemSource;
 import gov.ncbi.pmc.cite.TransformEngine;
 import gov.ncbi.pmc.ids.RequestId;
@@ -77,18 +84,45 @@ public class CasesTest {
             if (inReport.equals("nxml")) {
                 srcDoc = itemSource.retrieveItemNxml(rid);
             }
+            else if (inReport.equals("pub1")) {
+            	srcDoc = itemSource.retrieveItemPubOne(rid);
+            }
+            else {
+            	throw new BadParamException("Bad value in test-cases for 'inreport': " + inReport);
+            }
             log.trace("Source document:");
             log.trace(serializeXml(srcDoc));
 
             // Do the transformation
             Object result = engine.doTransform(srcDoc, transform);
+            Document resultDocument = null;
             log.trace("Transform result:");
-            log.trace(serializeXml((Document) result));
+            if (result instanceof String) {
+            	// result must be a json document
+            	String resultString = (String) result;
+            	log.trace(resultString);
+
+            	// use Jackson to convert it to an XML Document
+            	ObjectMapper jsonMapper = new ObjectMapper();
+            	JsonNode resultJson = jsonMapper.readTree(resultString);
+            	XmlMapper xmlMapper = new XmlMapper();
+            	String xmlString = xmlMapper.writeValueAsString(resultJson);
+            	log.trace("XML:\n" + xmlString);
+
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        	    DocumentBuilder builder = factory.newDocumentBuilder();
+        	    InputSource is = new InputSource(new StringReader(xmlString));
+        	    resultDocument = builder.parse(is);
+            }
+            else {
+            	resultDocument = (Document) result;
+                log.trace(serializeXml(resultDocument));
+            }
 
             URL schematronUrl = getClass().getClassLoader()
                 .getResource(testCase.validator + ".sch");
             assertTrue("Failed schematron validation",
-                validateXml(schematronUrl, (Document) result));
+                validateXml(schematronUrl, resultDocument));
         }
     }
 
